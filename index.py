ff31c3e63a8a31b4014418ffe32e799587a856c8
Siva <sivag0503@gmail.com>
2023-05-16 16:13:19 +0000

    init

--- index.py
+++ index.py
@@ -1,25 +1,33 @@
-from flask import Flask,render_template,request
-
+from flask import Flask,render_template,request,url_for,session,send_from_directory,redirect
+import ibm_db
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return render_template('login.html')
+conn = ibm_db.connect("DATABASE=bludb;HOSTHAME=0c77d6f2-5da9-48a9-81f8-86b520b87518.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31198;UID=rzm08887;PWD=NM02c9OExoYdTRH2",'','')
+print("connected")
 
-@app.route('/register',methods=['POST'])
-def register():
-	name  = request.form['name']
-	mail = request.form['mail']
-	password  = request.form['password']
-	context = {'name':name,'mail':mail,'password':password}
-	return render_template('register.html',data=context)
-
-@app.route('/login',methods=['POST'])
+@app. route("/login", methods=['POST', 'GET'])
 def login():
-	name  = request.form['name']
-	password  = request.form['password']
-	context = {'name':name}
-	return render_template('home.html',data=context)
+	msg = ''
+	if request .method == "POST":
+		USERNAME = request.form["username" ]
+		PASSWORD = request.form ["password" ]
+		sq1 = "SELECT * FROM USERN WHERE USERNAME=? AND PASSWORD=?"
+		stmt = ibm_db.prepare(conn, sq1)
+		ibm_db.bind_param(stmt, 1, USERNAME)
+		ibm_db.bind_param(stmt, 2, PASSWORD)
+		ibm_db.execute(stmt)
+		account = ibm_db.fetch_assoc(stmt)
+		print (account)
+	if account:
+		session['Loggedin'] = True
+		session['USERID'] = account['USERID']
+		session['USERNAME'] = account['USERNAME']
+		msg = "logged in successfully"
+		return redirect(url_for('home'))
+	else:
+		msg = "Incorrect Email/password"
+	return render_template('login.html',msg=msg)
+
 
 if __name__=="__main__":
 	app.run(debug=True)
